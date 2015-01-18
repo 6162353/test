572eaa6e067f9155c2e1aef392aab845f0c20778
k6162353 <6162353@gmail.com>
2015-01-18 13:11:54 +0000

    doing dz6

--- errors.php
+++ errors.php
@@ -1,13 +1,52 @@
 <?php
+header('Content-type: text/html; charset=utf-8');
+$otladka=1;
+
+$bd[][]=array();
+
+            if ($otladka) { 
+                                echo '<p><b>var_dump($bd)= </b></p>';
+        var_dump($bd);    
+        }
 
 
-$bd=array();
+$advertise=array( "title" => "Котенок", "price" => 200);
+
+            if ($otladka) { 
+                                echo '<p><b>var_dump($advertise)= </b></p>';
+        var_dump($advertise);    
+        }
+        
+
+/*
 
 for ($i=0; $i++; $i<count($bd); ) {
     
     var_dump($bd[i]);
 
-};
+};  */
 //Точка с запятой в конце условия цикла - ошибка
 
+$bd['count']=0;
 
+    
+            if ($otladka) { 
+                                echo '<p><b>var_dump($bd["count"])= </b></p>';
+        var_dump($bd['count']);    
+        }
+
+$bd['count']++;
+
+            if ($otladka) { 
+                                echo '<p><b>var_dump($bd["count"])= </b></p>';
+        var_dump($bd['count']);    
+        }
+        
+$bd[0]["advertise"]=$advertise;
+
+            if ($otladka) { 
+                                echo '<p><b>var_dump($bd)= </b></p>';
+        var_dump($bd);    
+        }
+        
+        
