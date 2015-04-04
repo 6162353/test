2098f94f566042d34d1ef7252a299f3ac0418349
6162353 <6162353@gmail.com>
2015-04-04 12:51:27 +0300

    commit

--- php_functions.php
+++ php_functions.php
@@ -7,9 +7,32 @@
  */
 header('Content-type: text/html; charset=utf-8');
 
-$categories = array ();
+$db_user='dz9';
+$db_name='dz9';
+$db_server='localhost';
 
-echo $categories; 
+
+$conn = mysql_connect(
+$db_server, $db_user,$db_user)
+or die("Невозможно установить соединение: ". mysql_error());
+
+
+echo '<br>conn=';
+echo $conn; 
+
+mysql_select_db($db_name);
+
+$query='select id from cities';
+
+$result_query = mysql_query($query) or die('Запрос не удался');
+
+echo '<br>result_query=';
+echo $result_query;
+
+$result = mysql_fetch_assoc($result_query);
+
+echo '<br>result=';
+vardump($result);
 
 
 ?>
